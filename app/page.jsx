e734a0571f4c866ecfdfb98d074f245346ecb7e6
Wiktor Jurkiewicz <watjurk@gmail.com>
2025-03-22 15:04:39 +0100

    remove irrelevant details

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import example from "./example.png";
 
 // You don't need to pass the whole ?20.scaled for this to happen. Using just '?' makes nextjs not run this import through client-phase loaders.
 // You can see that this image cannot be found in the .next/static/media folder...
-import exampleUserSpecifiedWidth from "./example.png?20.scaled";
+import exampleUserSpecifiedWidth from "./example.png?20";
 
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
--- types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.scaled" {
-	const content: string;
-	export default content;
-}
-
-declare module "*png" {
-	const content: string;
-	export default content;
-}
